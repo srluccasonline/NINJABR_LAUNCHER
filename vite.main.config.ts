bca433e9c8dbea46e12c57d876c4b47bf9f55d0b
Luccas Brandão <srluccasonline@gmail.com>
2025-12-07 09:38:42 -0300

    Tentando arruma patchright

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config
 export default defineConfig({
     build: {
-        rollupOptions: {
-            // Adicionei playwright-core por segurança
-            external: ['patchright', 'patchright-core'],
-        },
     },
 });
