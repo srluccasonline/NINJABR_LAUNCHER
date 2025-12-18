f500c55f94a5b65e3081cf3a1c93cb1bcb6d7eae
Luccas Brandão <srluccasonline@gmail.com>
2025-12-17 22:56:50 -0300

    Up 2.0.2

--- src/main.ts
+++ src/main.ts
@@ -58,11 +58,8 @@ const createWindow = () => {
   mainWindow.setMenu(null);
   mainWindow.on('page-title-updated', (e) => e.preventDefault());
 
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
-  }
+  // Restaurando a URL que apontava antigamente
+  mainWindow.loadURL("https://ninja-painel-dez-2025.vercel.app/");
 };
 
 app.on('ready', createWindow);
